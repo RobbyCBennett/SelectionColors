54f4ed9609f00c861081e5d19b8d8b707f5a8af7
Robby Bennett <robbycbennett@gmail.com>
2021-05-03 18:23:37 -0600

    Works on more sites

--- Code/applyColors.js
+++ Code/applyColors.js
@@ -1,28 +1,12 @@
-// New method to test:
-	// Dynamically create something called selectionColors.css and add it to document.styleSheets
-
-// Websites to test this out on:
-	// YouTube
-	// Stack Overflow
-	// Plutio
-
 chrome.storage.sync.get(null,
 	function (values) {
-		// Try to find a local stylesheet
-		if (values.bg || values.tx) {
-			localStyleSheetFound = -1;
-			for (var i = 0; i < document.styleSheets.length; i++) {
-				if (document.styleSheets[i].href == null) {
-					localStyleSheetFound = i;
-					break;
-				}
-			}
-			// if (localStyleSheetFound == -1) {
-			// 	console.log("Selection Colors is having trouble adding your style. It may cause issues on this website if the website checks for HTML changes.");
-			// }
-		}
-
-
+		// Create stylesheet
+		var sheet = (function() {
+			var style = document.createElement('style');
+			style.appendChild(document.createTextNode(''));
+			document.head.appendChild(style);
+			return style.sheet;
+		})();
 
 		// Background color
 		if (values.bg) {
@@ -33,20 +17,11 @@ chrome.storage.sync.get(null,
 					values.bgBri.toString() + "%, " +
 					(values.bgOpa / 100) +
 				") !important; }";
-			styleStringTag = "<style>" + styleString + "</style>";
 		}
 		else {
 			styleString = "body ::selection { background-color: inherit !important; }";
-			styleStringTag = "<style>" + styleString + "</style>";
 		}
-
-		if (localStyleSheetFound != -1) {
-			document.styleSheets[localStyleSheetFound].insertRule(styleString); // The sneaky method
-		}
-		else {
-			// document.body.innerHTML += styleStringTag; // Now all of China knows you're here!
-		}
-
+		sheet.insertRule(styleString);
 
 
 		// Text color
@@ -58,18 +33,10 @@ chrome.storage.sync.get(null,
 					values.txBri.toString() + "%, " +
 					(values.txOpa / 100) +
 				") !important; }";
-			styleStringTag = "<style>" + styleString + "</style>";
 		}
 		else {
 			styleString = "body ::selection { color: inherit !important; }";
-			styleStringTag = "<style>" + styleString + "</style>";
 		}
-
-		if (localStyleSheetFound != -1) {
-			document.styleSheets[localStyleSheetFound].insertRule(styleString);
-		}
-		else {
-			// document.body.innerHTML += styleStringTag;
-		}
+		sheet.insertRule(styleString);
 	}
 );
