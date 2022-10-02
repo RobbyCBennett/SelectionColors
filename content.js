63e4e5ff530fe8fc794e098c2cd260aed05cd6e6
Robby Bennett <robby@scitools.com>
2022-10-02 11:08:43 -0600

    Improve content script

--- content.js
+++ content.js
@@ -1,42 +1,36 @@
-chrome.storage.sync.get(null,
-	function (values) {
-		// Create stylesheet
-		var sheet = (function() {
-			var style = document.createElement('style');
-			style.appendChild(document.createTextNode(''));
-			document.head.appendChild(style);
-			return style.sheet;
-		})();
+function makeColor(hue, sat, bri, opa) {
+	return `hsla(${hue}, ${sat}%, ${bri}%, ${opa / 100})`;
+}
 
-		// Background color
-		if (values.bg) {
-			styleString =
-				"body ::selection { background-color: hsla(" +
-					values.bgHue.toString() + ", " +
-					values.bgSat.toString() + "%, " +
-					values.bgBri.toString() + "%, " +
-					(values.bgOpa / 100) +
-				") !important; }";
-		}
-		else {
-			styleString = "body ::selection { background-color: inherit !important; }";
-		}
-		sheet.insertRule(styleString);
+async function addStyle() {
+	// Get options
+	const options = await chrome.storage.sync.get();
 
+	// Get colors
+	const tx = options.tx ? makeColor(options.txHue, options.txSat, options.txBri, options.txOpa) : 'inherit';
+	const bg = options.bg ? makeColor(options.bgHue, options.bgSat, options.bgBri, options.bgOpa) : 'inherit';
 
-		// Text color
-		if (values.tx) {
-			styleString =
-				"body ::selection { color: hsla(" +
-					values.txHue.toString() + ", " +
-					values.txSat.toString() + "%, " +
-					values.txBri.toString() + "%, " +
-					(values.txOpa / 100) +
-				") !important; }";
+	// Create css rule
+	const rule = `
+		::selection,
+		body ::selection,
+		input::-webkit-datetime-edit-ampm-field:focus,
+		input::-webkit-datetime-edit-day-field:focus,
+		input::-webkit-datetime-edit-hour-field:focus,
+		input::-webkit-datetime-edit-millisecond-field:focus,
+		input::-webkit-datetime-edit-minute-field:focus,
+		input::-webkit-datetime-edit-month-field:focus,
+		input::-webkit-datetime-edit-second-field:focus,
+		input::-webkit-datetime-edit-week-field:focus,
+		input::-webkit-datetime-edit-year-field:focus {
+			color: ${tx} !important;
+			background-color: ${bg} !important;
 		}
-		else {
-			styleString = "body ::selection { color: inherit !important; }";
-		}
-		sheet.insertRule(styleString);
-	}
-);
+	`;
+
+	// Append stylesheet to body
+	const style = document.createElement('style');
+	document.head.appendChild(style);
+	style.sheet.insertRule(rule);
+}
+addStyle();
